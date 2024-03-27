3e2a9dab7ccb833e9965fe7723e127fa7ba040e8
DevYe <yesison714@gmail.com>
2024-03-27 12:33:52 -0500

    Ajustes adicionales

--- src/components/product/Product.tsx
+++ src/components/product/Product.tsx
@@ -13,9 +13,14 @@ import FormProduct from "../FormProduct/FormProduct";
 import ModalMobile from "../Modal/ModalFormProductMobile/ModalMobile";
 import FormProductMobile from "../FormProductMobile/FormProductMobile";
 
-
 export default function Product(dataFinal: any) {
-  const [finalData, setFinalData] = useState<any>([]);
+  const [finalData, setFinalData] = useState<any>({
+    name: "Nombre del producto",
+    description: "Descripción del producto",
+    price: 0,
+    videoUrl: "",
+    thumbnail: "/img/fondo_oscuro.jpg"
+  });
   const windowSize = UseWindowSize();
 
   useEffect(() => {
@@ -90,114 +95,143 @@ export default function Product(dataFinal: any) {
 
   return (
     <>
-      <div className={styles.mainVideo}>
-        <div className={styles.mainVideoContainer}>
-          <Video
-            src={finalData.videoUrl}
-            name={finalData.name}
-            price={finalData.price}
-            thumbnail={finalData.thumbnai}
-          />
-
-          <button onClick={handleOpenModal} className={styles.imageInfo}>
-            <Image src="/img/infoProduct.svg" alt="" width={50} height={50} />
-          </button>
-
-          <div className={styles.buttonAddListProduct} data-ripple-light="true" onClick={windowSize.width >= 767 ? handleOpenModalForm : handleOpenModalMobile}>
-            <button className={styles.button}>¡Comprar Ahora!</button>
-          </div>
-
-          <div className={styles.imageMain}>
-            <div className={styles.imageRounde}>
-              <div className={styles.imageRounderWraper}>
-                <img
-                  className={styles.imageCover}
-                  src={finalData.thumbnail}
-                  alt={`${finalData.name} thumbnail`}
-                />
-              </div>
-
-              <div className={styles.priceContainer}>
-                <div className={styles.priceText}>
-                  <span>
-                    Precio:{" "}
-                    <span className={styles.priceBold}>
-                      ${new Intl.NumberFormat().format(finalData.price)}
-                    </span>
-                  </span>
-                </div>
-                <div className={styles.separationfreeTextContainer}></div>
-                <div style={{ paddingTop: 8 }}>
-                  <span className={styles.freeTextContainer}>
-                    Envío: GRATIS
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className={styles.thumbnailContainer}>
-            <div className={styles.thumbnailRounde}>
-              <img
-                className={styles.thumbnailImage}
-                src={finalData.thumbnail}
-                alt={`${finalData.name} overlay thumbnail`}
+      {!finalData ? (
+        <div className={styles.loadingContent}>
+          <span>Loading...</span>
+        </div>
+      ) : (
+        <>
+          <div className={styles.mainVideo}>
+            <div className={styles.mainVideoContainer}>
+              <Video
+                src={finalData.videoUrl}
+                name={finalData.name}
+                price={finalData.price}
+                thumbnail={finalData.thumbnai}
               />
-            </div>
-            <div className={styles.containerTextImage}>
-              <div className={styles.textImageThumbanail}>
-                <p className="">{finalData.name}</p>
+
+              <button onClick={handleOpenModal} className={styles.imageInfo}>
+                <Image
+                  src="/img/infoProduct.svg"
+                  alt=""
+                  width={50}
+                  height={50}
+                />
+              </button>
+
+              <div
+                className={styles.buttonAddListProduct}
+                data-ripple-light="true"
+                onClick={
+                  windowSize.width >= 767
+                    ? handleOpenModalForm
+                    : handleOpenModalMobile
+                }
+              >
+                <button className={styles.button}>¡Comprar Ahora!</button>
+              </div>
+
+              <div className={styles.imageMain}>
+                <div className={styles.imageRounde}>
+                  <div className={styles.imageRounderWraper}>
+                    <img
+                      className={styles.imageCover}
+                      src={finalData.thumbnail}
+                      alt={`${finalData.name} thumbnail`}
+                    />
+                  </div>
+
+                  <div className={styles.priceContainer}>
+                    <div className={styles.priceText}>
+                      <span>
+                        Precio:{" "}
+                        <span className={styles.priceBold}>
+                          ${new Intl.NumberFormat().format(finalData.price)}
+                        </span>
+                      </span>
+                    </div>
+                    <div className={styles.separationfreeTextContainer}></div>
+                    <div style={{ paddingTop: 8 }}>
+                      <span className={styles.freeTextContainer}>
+                        Envío: GRATIS
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className={styles.thumbnailContainer}>
+                <div className={styles.thumbnailRounde}>
+                  <img
+                    className={styles.thumbnailImage}
+                    src={finalData.thumbnail}
+                    alt={`${finalData.name} overlay thumbnail`}
+                  />
+                </div>
+                <div className={styles.containerTextImage}>
+                  <div className={styles.textImageThumbanail}>
+                    <p className="">{finalData.name}</p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
 
-      <div className={styles.containerCart}>
-        <OnlyProductGrid catalog={dataFinal} />
-      </div>
+          <div className={styles.containerCart}>
+            <OnlyProductGrid catalog={dataFinal} />
+          </div>
 
-      <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
-        <div className={styles.ModalInfo}>
-          <h1>{finalData.name}</h1>
-          <span>{finalData.description}</span>
-        </div>
-        {finalData.attributes?.length > 0 && <div className={styles.containerVariation}>
-          {Object.entries(attributeValues).map(
-            ([attributeName, values]: any) => (
-              <div key={attributeName}>
-                <h3>{attributeName}:</h3>
-                <ul>
-                  {values.map((value: any, index: any) => (
-                    <li
-                      key={index}
-                      style={
-                        isColor(value)
-                          ? {
-                              color: value,
-                              background: value,
-                              width: "24px",
-                              height: "24px",
-                              borderRadius: "5px",
+          <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
+            <div className={styles.ModalInfo}>
+              <h1>{finalData.name}</h1>
+              <span>{finalData.description}</span>
+            </div>
+            {finalData.attributes?.length > 0 && (
+              <div className={styles.containerVariation}>
+                {Object.entries(attributeValues).map(
+                  ([attributeName, values]: any) => (
+                    <div key={attributeName}>
+                      <h3>{attributeName}:</h3>
+                      <ul>
+                        {values.map((value: any, index: any) => (
+                          <li
+                            key={index}
+                            style={
+                              isColor(value)
+                                ? {
+                                    color: value,
+                                    background: value,
+                                    width: "24px",
+                                    height: "24px",
+                                    borderRadius: "5px",
+                                  }
+                                : {}
                             }
-                          : {}
-                      }
-                    >
-                      {isColor(value) ? "" : value}
-                    </li>
-                  ))}
-                </ul>
+                          >
+                            {isColor(value) ? "" : value}
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  )
+                )}
               </div>
-            )
-          )}
-        </div>}
-      </Modal>
-      <ModalForm isOpen={isModalOpenForm} onClose={handleCloseModalForm}>
-        <FormProduct onSubmit={handleFormSubmit} dataFinal={finalData} />
-      </ModalForm>
-      <ModalMobile isOpen={isModalOpenMobile} onClose={handleCloseModalMobile}>
-        <FormProductMobile onSubmit={handleFormSubmit} dataFinal={finalData} />
-      </ModalMobile>
+            )}
+          </Modal>
+          <ModalForm isOpen={isModalOpenForm} onClose={handleCloseModalForm}>
+            <FormProduct onSubmit={handleFormSubmit} dataFinal={finalData} />
+          </ModalForm>
+          <ModalMobile
+            isOpen={isModalOpenMobile}
+            onClose={handleCloseModalMobile}
+          >
+            <FormProductMobile
+              onSubmit={handleFormSubmit}
+              dataFinal={finalData}
+            />
+          </ModalMobile>
+        </>
+      )}
     </>
   );
 }
